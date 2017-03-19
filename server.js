f2f1e643864a3ab45254f794f6d45e4ba838df51
iofirag <iofirag@gmail.com>
2017-03-19 14:57:12 +0200

    first commit

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-/************* Moduls **************/
-var express = require('express')
-
+var express = require('express');
 var app = express();
-app.use( express.static(__dirname + '/public'));// Serve files from ./www directory
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
-/******************** API ********************/
-// Serve web application
-app.get('/', function(req, res, next) {
-    res.sendFile('index.html', { root: __dirname+'/public'});
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
 });
 
-
-//-------------------------------------
-//// Configure server host+port
-app.set('host', process.env.HOST || 'localhost');
-app.set('port', process.env.PORT || 3333);
-
-app.listen(app.get('port'), function(){
-  console.log('Express server listening on ' + app.get('host') + ':' + app.get('port'));
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
 });
